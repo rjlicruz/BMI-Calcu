3284eabdf4bce3e4f9a1b0b56b024b7e0d05241c
rjlicruz <rjlicruz@gmail.com>
2021-01-12 16:37:35 +0800

    2 files modified, 1 added

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -3,33 +3,31 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  styleUrls: ['home.css']
 })
 export class HomePage {
-  height: number;
+height: number;
   weight: number;
   bmi: number;
   bmiMessage: string;
 
-  constructor(public navCtrl: NavController) {}
+ constructor(public navCtrl: NavController) {}
 
-  calculateBMI() {
-      this.bmi = this.weight / (this.height * this.height);
-      this.bmi = parseFloat(this.bmi.toFixed(2));
+ calculateBMI() {
+    this.bmi = this.weight / (this.height * this.height);
+    this.bmi = parseFloat(this.bmi.toFixed(2));
 
-      if (this.bmi < 18.5) {
-        this.bmiMessage = "Underweight";
-      } else if (this.bmi > 18.5 && this.bmi < 25) {
-        this.bmiMessage = "Normal"
-      } else if (this.bmi > 25 && this.bmi < 30) {
-        this.bmiMessage = "Overweight";
-      } else {
-        this.bmiMessage = "Obese";
-      }
+    if (this.bmi < 18.5) {
+      this.bmiMessage = "Underweight";
+  } else if (this.bmi > 18.5 && this.bmi < 25) {
+    this.bmiMessage = "Normal";
+  } else if (this.bmi > 25 && this.bmi < 30) {
+    this.bmiMessage = "Overweight";
+  } else {
+    this.bmiMessage = "Obese";
   }
-  
+ 
+  }
 
-  
 }
-
-
